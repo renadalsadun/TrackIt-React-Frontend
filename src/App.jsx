4d0f668255cb49285a1d01de8fcbad4c48b6f967
renad <xirnzsixteen@hotmail.com>
2025-05-05 20:46:22 +0300

    add login page and logout function

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,26 @@ import ApplicationUpdate from './pages/ApplicationUpdate'
 import DocumentAdd from './pages/DocumentAdd'
 import DocumentList from './components/DocumentList/DocumentList'
 import Signup from './pages/SignUp'
+import Login from './pages/Login'
 
 
 
 function App() {
 
+  function logout(){
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('refresh_token')
+    window.location.href = '/'
+  }
+
+
   return (
     <>
     <Router>
         <Routes>
+          <Route path='/' element={<h1>WELCOME</h1>}/>
           <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/Home' element={<Home />} />
           <Route path='trackers/:id' element={<TrackerDetail/>}/>
           <Route path='trackers/add' element={<TrackerAdd/>}/>
