fd539124d30250116a67ee9f16311c36ee3c52a8
renad <xirnzsixteen@hotmail.com>
2025-05-06 00:52:52 +0300

    added authorization to requests in Tracker Add Page

--- src/pages/TrackerAdd.jsx
+++ src/pages/TrackerAdd.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+
 import TrackerForm from '../components/TrackerForm/TrackerForm'
+import { authorizedRequest } from '../lib/api'
 
 
-//// need to handel the user
+
 function TrackerAdd() {
     
     const FIELDS_LIST = [
@@ -17,14 +19,16 @@ function TrackerAdd() {
     const [checkedFields , setCheckedFields] = useState([])
     
     async function handleSubmit(event){
-        // we will get a Promise instead of the result if we did not use async
-        // and to solve CORS issue add to django ()
 
         event.preventDefault()
         const fields = checkedFields
-        const payload = {name, fields, user:1} ////MUST HANDEL USERS LATER!
-        const url = 'http://127.0.0.1:8000/api/trackers/new/'
-        const response = await axios.post(url, payload)
+        const payload = {name, fields}
+        const response = await authorizedRequest(
+            'post',
+            `trackers/new/`,
+            payload
+        )
+        
         console.log(response)
         // setName('')
         // setCheckedFields('')
