8b174abbb36b871c5d33811b92b92e5351b98f97
renad <xirnzsixteen@hotmail.com>
2025-05-07 02:58:06 +0300

    add error handling to Application update

--- src/pages/ApplicationUpdate.jsx
+++ src/pages/ApplicationUpdate.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React from 'react'
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 
 import { authorizedRequest } from '../lib/api'
@@ -10,72 +10,90 @@ import ApplicationForm from '../components/ApplicationForm/ApplicationForm'
 
 function ApplicationUpdate() {
     // i will have the tracker id and the application id 
-    const DATE_FIELDS = [ 'date_applied' , 'deadline' , 'interview_date' , 'start_date' , 'end_date' ]
+    const DATE_FIELDS = ['date_applied', 'deadline', 'interview_date', 'start_date', 'end_date']
     const { trackerId, applicationId } = useParams();
     const [formFields, setFormFields] = useState({})
     const [fields, setFields] = useState([])
-    const [tracker, setTracker] = useState({})
-    const [application, setApplication] = useState({})
-    
-    async function getFields(){
+
+
+    //error handling
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+
+
+    async function getFields() {
         //get the tracker object using id
         //get its fields attr
-        const response = await authorizedRequest(
-            'get',
-            `trackers/${trackerId}/`
-        )
-        setFields(response.data.fields)
-        setTracker(response.data)
-        console.log(response.data.fields)
+        try {
+            const response = await authorizedRequest(
+                'get',
+                `trackers/${trackerId}/`
+            )
+            setFields(response.data.fields)
+            setLoading(false)
+        }
+        catch (error) {
+            setError('Something went wrong. Please try again later')
+            setLoading(false)
+        }
     }
 
-    async function getApplication(){
+    async function getApplication() {
         //get the application object using id
         //get its attrs
-        const response = await authorizedRequest(
-            'get',
-            `applications/${applicationId}/`
-        )
-        console.log(response.data)
-        setApplication(response.data)
-        setFormFields(response.data)
+        try{
+            const response = await authorizedRequest(
+                'get',
+                `applications/${applicationId}/`
+            )
+            setFormFields(response.data)
     
+        }
+        catch(error){
+            setError('Something went wrong. Please try again later')
+        }
+
     }
 
-    useEffect( () => { 
+    useEffect(() => {
         getFields()
         getApplication()
-    } ,[] )
+    }, [])
 
 
 
-    async function handleSubmit(event){
+    async function handleSubmit(event) {
         event.preventDefault()
-        const payload = { ...formFields, tracker:trackerId }
-        const response = await authorizedRequest(
-            'patch',
-            `applications/${applicationId}/update/`,
-            payload
-        )
-        console.log(response)
+        const payload = { ...formFields, tracker: trackerId }
+        try{
+            const response = await authorizedRequest(
+                'patch',
+                `applications/${applicationId}/update/`,
+                payload
+            )
+    
+        }
+        catch{
+            setError('Something went wrong. Please try again later')
+        }
     }
 
 
-// formTitle handleSubmit fields DATE_FIELDS formFields setFormFields submitButtonText
-
     return (
-    <div>
-        <h2> ApplicationUpdate </h2>
-        <ApplicationForm 
-            formTitle='Update Application'
-            fields = {fields}
-            DATE_FIELDS = {DATE_FIELDS}
-            formFields = {formFields}
-            setFormFields = {setFormFields}
-            submitButtonText = 'Update'
-            handleSubmit = {handleSubmit}
-        />
-    </div>
+        <div>
+            <h2> ApplicationUpdate </h2>
+            {error?(<p>{error}</p>):{}}
+            <ApplicationForm
+                formTitle='Update Application'
+                fields={fields}
+                DATE_FIELDS={DATE_FIELDS}
+                formFields={formFields}
+                setFormFields={setFormFields}
+                submitButtonText='Update'
+                handleSubmit={handleSubmit}
+            />
+        </div>
     )
 }
 
