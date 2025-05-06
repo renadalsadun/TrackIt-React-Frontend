2b2721b343f2a7548353d10545a74f6150fd0ff1
renad <xirnzsixteen@hotmail.com>
2025-05-07 01:06:14 +0300

    link documents to applications M:N in frontend

--- src/components/ApplicationForm/ApplicationForm.jsx
+++ src/components/ApplicationForm/ApplicationForm.jsx
@@ -1,39 +1,56 @@
 import axios from 'axios'
-import React, {useEffect, useState } from 'react'
-import { useNavigate , useParams } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
+
+import { authorizedRequest } from '../../lib/api'
 
 
 
 function ApplicationForm(props) {
 
 
-
+    const [documents, setDocuments] = useState([])
     const navigate = useNavigate() //there is an issue here i guess, maybe because i didnot use buttons 
 
 
-    function handelCancel(){
+    async function getAllDocuments() {
+        const response = await authorizedRequest(
+            'get',
+            `documents/`
+        )
+        console.log(response)
+        setDocuments(response.data)
+    }
+
+    useEffect(() => {
+        getAllDocuments()
+    }, [])
+
+
+
+    function handelCancel() {
         navigate(-1)
     }
 
 
 
-    return ( 
+    return (
         <div>
             <h3>{props.formTitle}</h3>
             <form onSubmit={props.handleSubmit}>
                 {
-                    
-                    props.fields.map(field =>{
-                        const type = props.DATE_FIELDS.includes(field) ?  'date' : 'text'
-                        if (field === 'priority'){
-                            return(
+
+                    props.fields.map(field => {
+                        const type = props.DATE_FIELDS.includes(field) ? 'date' : 'text'
+                        if (field === 'priority') {
+                            return (
                                 <>
                                     <label htmlFor={field}>{field} </label>
                                     <select
                                         id={field}
                                         name={field}
-                                    
-                                        value = {props.formFields[field]}
+
+                                        value={props.formFields[field]}
                                         onChange={event =>
                                             props.setFormFields({
                                                 ...props.formFields,
@@ -41,30 +58,67 @@ function ApplicationForm(props) {
                                             })
                                         }
                                     >
-                                    <option value="C">Critical</option>
-                                    <option value="H">High</option>
-                                    <option value="M">Medium</option>
-                                    <option value="L">Low</option>
+                                        <option value="C">Critical</option>
+                                        <option value="H">High</option>
+                                        <option value="M">Medium</option>
+                                        <option value="L">Low</option>
                                     </select>
 
                                 </>
                             )
                         }
-                        return(
+                        if (field === 'documents') {
+                        
+                            return (
+                                <>
+                                    <label htmlFor={field}>{field} </label>
+                                    <select
+                                        multiple // source : w3school
+                                        id={field}
+                                        name={field}
+
+                                        value={props.formFields[field] || []}
+                                        onChange={event =>{
+                                            const selectedDocuments = []
+                                            for (let i=0; i<event.target.selectedOptions.length; i++){
+                                                selectedDocuments.push(event.target.selectedOptions[i].value)
+                                            }
+                                            props.setFormFields({
+                                                ...props.formFields,
+                                                [field]: selectedDocuments
+                                            })}
+                                        }
+                                    >
+                                        {documents.length ? (
+                                            documents.map(document => (
+                                                <option key={document.id} value={document.id}>
+                                                    {document.name}
+                                                </option>
+                                            ))
+                                        ) : (
+                                            <option disabled>No Documents Found</option>
+                                        )}
+
+                                    </select>
+
+                                </>
+                            )
+                        }
+                        return (
                             <>
                                 <label htmlFor={field}>{field} </label>
                                 <input
-                                id={field}
-                                name={field}// as our django app expects!
-                                type = {type}
-                                required
-                                value={props.formFields[field]}
-                                onChange={event =>
-                                    props.setFormFields({
-                                        ...props.formFields,
-                                        [field]: event.target.value
-                                    })
-                                }
+                                    id={field}
+                                    name={field}// as our django app expects!
+                                    type={type}
+                                    required
+                                    value={props.formFields[field]}
+                                    onChange={event =>
+                                        props.setFormFields({
+                                            ...props.formFields,
+                                            [field]: event.target.value
+                                        })
+                                    }
                                 />
                             </>
                         )
