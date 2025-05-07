58745be5e2083602a1350ec2fc7f7023350bf5d9
renad <xirnzsixteen@hotmail.com>
2025-05-07 03:18:55 +0300

    add error handling to Tracker Update

--- src/pages/TrackerUpdate.jsx
+++ src/pages/TrackerUpdate.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React from 'react'
-import { useState , useEffect } from 'react'
-import { useNavigate , useParams } from 'react-router'
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router'
 
 import { authorizedRequest } from '../lib/api'
 import TrackerForm from '../components/TrackerForm/TrackerForm'
@@ -11,65 +11,85 @@ import TrackerForm from '../components/TrackerForm/TrackerForm'
 function TrackerUpdate(props) {
 
     const FIELDS_LIST = [
-        'company' , 'organization' , 'university' , 'location' , 'website' , 'link' , 
-        'description' , 'referral' , 'contact' , 'notes' , 'priority' , 'date_applied' ,
-        'deadline' , 'interview_date' , 'start_date' , 'end_date' , 'documents'
-        ]
+        'company', 'organization', 'university', 'location', 'website', 'link',
+        'description', 'referral', 'contact', 'notes', 'priority', 'date_applied',
+        'deadline', 'interview_date', 'start_date', 'end_date', 'documents'
+    ]
 
     const { id } = useParams()
     const [name, setName] = useState('')
-    const [checkedFields , setCheckedFields] = useState([])
+    const [checkedFields, setCheckedFields] = useState([])
     const navigate = useNavigate()
+    const [error, setError] = useState('') //error handling
 
 
 
     //to get the clicked Tracker's data from its id!
     async function getCurrentResponceData() {
-        const response = await authorizedRequest(
-            'get',
-            `trackers/${id}/`
-        )
-        setName(response.data.name)
-        setCheckedFields(response.data.fields)
+        try {
+
+            const response = await authorizedRequest(
+                'get',
+                `trackers/${id}/`
+            )
+            setName(response.data.name)
+            setCheckedFields(response.data.fields)
+        }
+        catch {
+            if (err.response?.status === 404) {
+                navigate('/not-found')
+            } else {
+                setError('Failed to fetch tracker. Please try again later')
+            }
+
+        }
     }
 
 
-    useEffect(()=>{
-        getCurrentResponceData()},[])
+    useEffect(() => {
+        getCurrentResponceData()
+    }, [])
 
 
 
-    async function handleSubmit(event){
+    async function handleSubmit(event) {
         // 200 : 400
 
         event.preventDefault()
         const fields = checkedFields
-        const payload = {name, fields} 
-        const response = await authorizedRequest(
-            'patch',
-            `trackers/${id}/update/`,
-            payload
-        )
+        const payload = { name, fields }
+        try {
 
-        console.log(response)
-        navigate(`/trackers/${id}`)
+            const response = await authorizedRequest(
+                'patch',
+                `trackers/${id}/update/`,
+                payload
+            )
+
+            navigate(`/trackers/${id}`)
+        }
+        catch{
+            setError('Failed to update tracker. Please try again')
+        }
     }
 
 
     return (
-    <div>
-        <h2> TrackerUpdate </h2>
-        <TrackerForm 
-            name = {name}
-            FIELDS_LIST = {FIELDS_LIST}
-            setName = {setName}
-            checkedFields = {checkedFields}
-            setCheckedFields = {setCheckedFields}
-            formTitle={`Edit ${name} Tracker`}
-            submitButtonText = 'Done'
-            handleSubmit = {handleSubmit}
-        />
-    </div>
+        <div>
+            <h2> TrackerUpdate </h2>
+            {error ? (<p>{error}</p>) : {}}
+
+            <TrackerForm
+                name={name}
+                FIELDS_LIST={FIELDS_LIST}
+                setName={setName}
+                checkedFields={checkedFields}
+                setCheckedFields={setCheckedFields}
+                formTitle={`Edit ${name} Tracker`}
+                submitButtonText='Done'
+                handleSubmit={handleSubmit}
+            />
+        </div>
     )
 }
 
